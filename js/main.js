0095af8be5a941c6a6fab36426d8dffcb3695903
Thiago Ribeiro <thiagogr71@gmail.com>
2022-11-07 15:57:23 -0300

    feature: create effect onkeyup

--- js/main.js
+++ js/main.js
@@ -23,10 +23,28 @@ themeSelector.parentElement.addEventListener("click", ()=>{
     themeSelector.animate(animationKeyframe, 250)
 });
 
+const activeKeyBg = {
+    theme0: ["white", "hsla(6, 93%, 67%, 1)", "hsl(224, 51%, 76%)"], 
+    theme1:["white", "hsl(25, 100%, 61%)", "hsl(185, 41%, 56%)"], 
+    theme2:["hsla(268, 54%, 44%, 1)", "hsla(177, 100%, 79%, 1)", "hsla(280, 56%, 44%, 1)"]
+}
+
+function animateKey (key, typeKey) {
+   const currentTheme = theme.getAttribute("href").match(/theme\d/)[0];
+   const button = document.getElementById(key)
+   const normalBg = button.style.backgroundColor
+   button.style.backgroundColor = activeKeyBg[currentTheme][typeKey-1]
+   setTimeout(() => {
+       button.style.backgroundColor = normalBg
+    }, 300);
+}
+
 function keyVerifier (key){
     let operation = displayValue.textContent;
     if(key.match(/(^[0-9]|\.)/)){
         key = key.replace('.', ',');
+        animateKey(key, 1);
+
         let operationArray = operation.split(' ');
         if(key.match(/,/)){
             if(!operationArray[operationArray.length-1].match(/,|\s/)){
@@ -43,19 +61,23 @@ function keyVerifier (key){
     }else if (key.match(/\/|\+|\*|-/)){
         displayValue.textContent == 0 ? null:
         operation.slice(-2, -1).match(/\/|\+|\*|-/) ? displayValue.textContent = operation.replace(operation.slice(-2, -1), key): 
-        displayValue.textContent += ` ${key} `
+        displayValue.textContent += ` ${key} `;
+        animateKey(key, 1);
     } else if (key == "Backspace") {
         displayValue.textContent.length == 1 ? displayValue.textContent = 0 : 
         operation.slice(-1) == ' ' ? 
         displayValue.textContent = operation.slice(0, -3):
         displayValue.textContent = operation.slice(0,-1);
+        animateKey(key, 3);
     } else if (key == "Delete") {
         displayValue.textContent = 0;
+        animateKey(key, 3);
     }else if (key == "Enter" || key == "="){
         operation = operation.replace(/,/g, '.');
         let result = Function(`return ${operation}`)();
         displayValue.textContent = result.toString().replace('.', ',');
         result.toString().length >= 18 ? displayValue.style.fontSize = "0.8rem" : null
+        animateKey(key, 2);
    } 
 }
 
